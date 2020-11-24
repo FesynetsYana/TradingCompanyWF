e2eaa974c864f155533afb4fa20840a46280b48a
FesynetsYana <01032001fesynets@gmail.com>
2020-11-24 23:52:07 +0200

    Continue work with WPF project

--- TopicWPF/MainWindow.xaml.cs
+++ TopicWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DTO;
 using BusinessLogic.Concrete;
+using DAL.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,14 +23,48 @@ namespace TopicWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UserManager userManager;
+        private UserDal userDal;
+        private string connectionString = "Data Source=localhost;Initial Catalog=ManagerNews;Integrated Security=True";
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            userDal = new UserDal(connectionString);
         }
+        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            uint isLogIn = 0;
+            UserDTO user = new UserDTO();
+            user.Login = login.Text;
+           // user.Password = PasswordBox.Text;
+            isLogIn = userDal.LogIn(user);
 
+            if (isLogIn != 0)
+            {
+                if (isLogIn == 1)
+                {
+                    user.Email = connectionString;
+                    userManager = new ClientManager(user);
+                }
+                else
+                {
+                    user.Email = connectionString;
+                    userManager = new AdminManager(user);
+                }
+                TopicList tp = new TopicList(userManager);
+                this.Visible = false;
+                tp.Show();
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ TopicWPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLogic.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TopicWPF.ViewModels
+{
+    public class LoginViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+        public string _username;
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            private get;
+            set;
+        }
+
+        private readonly UserManager _security;
+
+        public LoginViewModel(UserManager security)
+        {
+            _security = security;
+        }
+
+        public bool Login()
+        {
+            return _security.Login(Username, Password);
+        }
+
+    }
+
+}
